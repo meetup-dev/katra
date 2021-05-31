f820f131fc7f510de9d0a4c651d682b1b9ca79df
Derek Bourgeois <derek@devoption.io>
2021-05-31 18:31:57 -0400

    enable dark mode

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>{{ config('app.name') }}</title>
+        
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body class="dark:bg-gray-900 bg-gray-100">
+        
+    </body>
+</html>
